7c05b5eb57b1ed20f819649d7721a1c7d9b3f1b3
Syed Munawar Razvi <34087445+RazviMunawar@users.noreply.github.com>
2021-09-03 01:40:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Plan from './Components/Plan';
 
 function App() {
+  const [text, setText] = useState('');
+  const [items, setItems] = useState([]);
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  }
+
+  const handleAdd = () => {
+    if(text !== ""){
+      setItems([...items, text]);
+      setText("");
+    }
+  }
+
+  const handleDelete = (id) => {
+    const newItem = items.filter( (ele, i) => {
+      return i !== id
+    });
+    setItems([...newItem]);
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid my-5">
+      <div className="row">
+        <div className="col-sm-6 mx-auto text-white shadow-lg p-3">
+          <h2 className="text-center">To Do List</h2>
+          <div className="row">
+            <div className="col-9">
+              <input type="text" className="form-control" placeholder="Write plan here..." onChange={ handleChange } value={ text } />
+            </div>
+            <div className="col-2">
+              <button className="btn btn-warning px-5 font-weight-bold" onClick={ handleAdd }>Add</button>
+            </div>
+            <div className="container-fluid">
+              {/* <ul className="list-unstyled row m-5">
+                {
+                  items.map( (value, i) => {
+                    return <Plan sendData={ handleDelete } key={i} id={ i } value={value} />
+                  })
+                }
+              </ul> */}
+              <ul className="list-unstyled row m-5">
+                <Plan sendData={ handleDelete } items={items} />
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Plan.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Plan = ( { items, sendData } ) => {
+    return (
+        <>
+            {
+                items.map( (val, index) => {
+                    return(
+                        <React.Fragment key={ index }>
+                           <li key={ index } className="shadow p-2 my-2 col-sm-9">{val}</li>
+                           <button className="btn-danger my-2 offset-1 col-sm-2" onClick={ () => sendData(index) }>X</button> 
+                        </React.Fragment>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+export default Plan
